92675422e8d3aab9152453e33b7bd440aa9c2b3e
Cauctoflo <87550087+cauctoflo@users.noreply.github.com>
2024-02-12 20:23:22 +0100

    Update

--- quiz_main.php
+++ quiz_main.php
@@ -1,39 +1,47 @@
 <?php
 session_start();
 
+// Vérifier la connexion de l'utilisateur
 if (!isset($_SESSION['Login']) || $_SESSION['Login'] !== true) {
     header('Location: login.php');
     exit();
 }
 
+// Initialiser les variables de session si nécessaire
 if (!isset($_SESSION['current_question'])) {
     $_SESSION['current_question'] = 0;
     $_SESSION['Points'] = 0;
 }
-if (!empty($questions) && is_array($questions)) {
-    $questions_available = true;
-}
-require_once('jsonclass.php');
 
+// Charger les questions à partir du fichier JSON
+require_once('jsonclass.php');
 $file_path = 'json/questions.json';
 $jsonFileManager = new JsonFileManager($file_path);
 $questions = $jsonFileManager->read();
+
+// Vérifier si des questions sont disponibles
+$questions_available = !empty($questions) && is_array($questions);
 $affichage = false;
+// Traitement du formulaire
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $affichage = true;
+
     if (isset($_POST['answer'])) {
+        $affichage = true;
         $question_id = $_SESSION['current_question'];
         $selected_answer = $_POST['answer'];
-        $correct_answer = $questions[$question_id]['correct_answer'];
+
+        // Récupérer la question actuelle
+        $question = $questions[$question_id];
+
+        // Récupérer la réponse correcte pour la question actuelle
+        $correct_answer = $question['correct_answer'];
+
 
         if ($selected_answer == $correct_answer) {
             $_SESSION['Points'] += 30;
             $message = '<div class="alert alert-success" role="alert">Bonne réponse ! Vous avez gagné 30 points.</div>';
-
-
         } else {
-            $message = '<div class="alert alert-danger" role="alert">Mauvaise réponse. La réponse correcte était : ' . $questions[$question_id]['answers'][$correct_answer] . '</div>';
-
+            $message = '<div class="alert alert-danger" role="alert">Mauvaise réponse. La réponse correcte était : ' . $question['answers'][intval($correct_answer)] . '</div>';
         }
     } elseif (isset($_POST['next_question'])) {
         $_SESSION['current_question']++;
@@ -55,18 +63,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link rel="stylesheet" href="css/main_quiz.css"> <!-- Import du CSS personnalisé -->
 </head>
 <body>
-<?php
-include 'navbar.php';
-?>
+<?php include 'navbar.php'; ?>
 <div class="containers text-center">
     <?php
-
-
-
     $question_id = $_SESSION['current_question'];
-    if ($question_id < count($questions)) {
+    if ($questions_available && $question_id < count($questions)) {
         $question = $questions[$question_id];
-
         ?>
         <div>
             <div class="question" style="padding: 50px;">
@@ -75,52 +77,60 @@ include 'navbar.php';
             </div>
             <form method="post">
                 <input type="hidden" name="question_id" value="<?php echo $question_id; ?>">
-                <div class="input-container"> <!-- Container pour les inputs radio -->
-                    <?php foreach ($question['answers'] as $key => $answer) : ?>
-                        <div class="answer-group">
-                            <input type="radio" id="answer_<?php echo $key; ?>" name="answer" value="<?php echo $key; ?>" class="input-radio">
-                            <label class="input-label" for="answer_<?php echo $key; ?>"><?php echo $answer; ?></label>
-                        </div>
-                    <?php endforeach; ?>
+                <div class="input-container">
+                    <?php
+                    // Affichage des réponses en fonction du type de question
+                    switch ($question['type']) {
+                        case 'radio':
+                            foreach ($question['answers'] as $key => $answer) {
+                                echo '<div class="answer-group">';
+                                echo '<input type="radio" id="answer_' . $key . '" name="answer" value="' . $key . '" class="input-radio">';
+                                echo '<label class="input-label" for="answer_' . $key . '">' . $answer . '</label>';
+                                echo '</div>';
+                            }
+                            break;
+                        case 'check':
+                            foreach ($question['answers'] as $key => $answer) {
+                                echo '<div class="answer-group">';
+                                echo '<input type="checkbox" id="answer_' . $key . '" name="answer" value="' . $key . '" class="input-checkbox">';
+                                echo '<label class="input-label" for="answer_' . $key . '">' . $answer . '</label>';
+                                echo '</div>';
+                            }
+                            break;
+                        case 'dropdown':
+                            echo '<select name="answer">';
+                            foreach ($question['answers'] as $key => $answer) {
+                                echo '<option value="' . $key . '">' . $answer . '</option>';
+                            }
+                            echo '</select>';
+                            break;
+                    }
+                    ?>
                 </div>
-                <input type="text" hidden="hidden" id="affichage" value="true">
+                <?php if (isset($message)) : ?>
+                    <p><?php echo $message; ?></p>
+                <?php endif; ?>
                 <button type="submit" class="btn btn-danger">Soumettre la réponse</button>
             </form>
-            <?php if (isset($message)) : ?>
-                <p><?php echo $message; ?></p>
-            <?php endif; ?>
-        </div>
-        <form method="post" action="">
-
-            <?php if ($question_id == count($questions) - 1) :
-                if ($affichage == true) :
-                    $affichage = false;?>
-
-
+            <?php if ($question_id == count($questions) - 1) : ?>
+                <form method="post" action="">
                     <input type="hidden" name="quiz_finish" value="true">
                     <button type="submit" class="btn btn-danger">Finir le quiz</button>
-                <?php endif; ?>
+                </form>
+            <?php if ($affichage == true) ?>
+                <form method="post" action="">
+                    <input type="hidden" name="next_question" value="true">
+                    <button type="submit" class="btn btn-danger">Question suivante</button>
+                </form>
             <?php endif; ?>
-            <?php if ($affichage == true) : ?>
-                <input type="hidden" name="next_question" value="true">
-                <button type="submit" class="btn btn-danger">Question suivante</button>
-
-            <?php endif; ?>
-        </form>
-
+        </div>
         <?php
     } else {
-
         header('Location: quiz_finish.php'); // Rediriger vers une page de confirmation de fin de quiz
         exit();
     }
-?>
-
-
+    ?>
 </div>
-
 </body>
-<?php
-include_once 'footer.php'
-?>
+<?php include_once 'footer.php' ?>
 </html>
